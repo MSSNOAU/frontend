525d9fe3129fc30576f29bfe4dad7c14cf8e2b6d
Tiskae <adedokuntobiloba100@gmail.com>
2024-10-30 07:06:17 +0100

    feat: press page

---
+++ src/app/press/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import forAllUsers from "@/src/shared/hocs/forAllUsers";
+import React from "react";
+
+const PressDetails = () => {
+  return <></>;
+};
+
+export default forAllUsers(PressDetails);
